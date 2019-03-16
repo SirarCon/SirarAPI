87c4603c9c7b1fd0e522da13f179aa950fbab83d
Sirar <sirar2018@hotmail.com>
2019-03-16 14:02:47 -0600

    CRUD de paises y fases

--- api/controllers/RecursoController.js
+++ api/controllers/RecursoController.js
@@ -8,6 +8,28 @@ var globales =  require("../Globales.js");
 //#endregion Requires
 
 //#region Pais
+exports.crearPais = function(req, res){
+    var nuevoPais = new Pais(req.body);
+     nuevoPais.save().then(pais => {
+        res.json({token: res.locals.token, datos: globales.mensajes(-4, "Pais", req.body.name).instance});
+      
+    }).catch(err => {                
+          res.json({token: res.locals.token, datos: globales.mensajes(10, "País.", funcionesGlobales.manejarError(err)).instance});
+    })
+}
+
+exports.borrarPais = function(req, res){
+    Pais.findOneAndRemove({_id: req.body.idPais}).exec().then(pais => {
+        if(pais){
+            res.json({token: res.locals.token, datos: globales.mensajes(-2, "País", " ").instance});    
+        }else{
+            res.json({token: res.locals.token, datos: globales.mensajes(2, "País", " ").instance});
+        }
+    }).catch(err=>{
+        res.json({token: res.locals.token, datos: globales.mensajes(20, "país.", funcionesGlobales.manejarError(err)).instance});        
+    })
+}
+
 exports.obtenerPaises = function(req, res){
     Pais.find().sort({nombre : 1}).exec().then(paises =>{
             res.json({token: res.locals.token, datos: globales.mensajes(-1, null, null, paises).instance});          
@@ -18,6 +40,27 @@ exports.obtenerPaises = function(req, res){
 //#endregion Pais
 
 //#region Fase
+exports.crearFase = function(req, res){
+    var nuevaFase = new Fase(req.body);
+     nuevaFase.save().then(fase => {
+        res.json({token: res.locals.token, datos: globales.mensajes(-4, "Fase", req.body.descripcion).instance});
+    }).catch(err => {                
+          res.json({token: res.locals.token, datos: globales.mensajes(10, "Fase.", funcionesGlobales.manejarError(err)).instance});
+    })
+}
+
+exports.borrarFase = function(req, res){
+    Fase.findOneAndRemove({_id: req.body.idFase}).exec().then(fase => {
+        if(fase){
+            res.json({token: res.locals.token, datos: globales.mensajes(-2, "Fase", " ").instance});    
+        }else{
+            res.json({token: res.locals.token, datos: globales.mensajes(2, "Fase", " ").instance});
+        }
+    }).catch(err=>{
+      res.json({token: res.locals.token, datos: globales.mensajes(20, "fase.", funcionesGlobales.manejarError(err)).instance});        
+    })
+}
+
 exports.obtenerFases = function(req, res){
     Fase.find().sort({ posicion: 1 }).exec().then(fases=>{
             res.json({token: res.locals.token, datos: globales.mensajes(-1, null, null, fases).instance});            
--- api/routes/RecursoRoute.js
+++ api/routes/RecursoRoute.js
@@ -4,15 +4,36 @@ module.exports = function(app, express) {
     var recursoController = require('../controllers/RecursoController');
     var seguridad= require("./Seguridad.js");
     var routerGeneral = express.Router()
+    var routerAdm = express.Router()
 
 //Se le assignan los middleware a los usuarios adm antes del login
 routerGeneral.use(seguridad.verificarTokenGeneral);
 
+//Se le assignan los middleware a los router de Adm luego del Login
+routerAdm.use(seguridad.verificarTokenGeneral, seguridad.verify);
+
+ 
+//Rutas POST, GET, PUT, DELETE
 routerGeneral.route('/paises')
 .get(recursoController.obtenerPaises);
 
 routerGeneral.route('/fases')
 .get(recursoController.obtenerFases);
 
+routerAdm.route('/paises')
+.post(recursoController.crearPais)
+
+routerAdm.route('/pais')
+.delete(recursoController.borrarPais)
+
+routerAdm.route('/fases')
+.post(recursoController.crearFase)
+
+routerAdm.route('/fase')
+.delete(recursoController.borrarFase)
+
+var routerAdm = express.Router()
+
+app.use("/api", routerAdm);
 app.use("/", routerGeneral);
 };
