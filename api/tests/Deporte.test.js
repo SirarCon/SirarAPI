195d90b6d1fc6a14befe232a7db8a0186e75bb3c
Sirar <sirar2018@hotmail.com>
2020-03-19 23:26:10 -0600

    Tests escenario positivo deportes

--- api/tests/Deporte.test.js
+++ api/tests/Deporte.test.js
@@ -7,13 +7,35 @@ prueba = require("../models/PruebaModel"),
 controlador = require("../controllers/DeporteController"),
 expressRequestMock = require('express-request-mock');
 
-var body = {
-        nombre: "Natación",
-        activo: true
-};
-var response = body;
 
-describe('Competencia Atleta', () => {
+
+describe('Federación', () => {
+
+    var body = {
+        nombre: "Federación de Natación",
+        activo: true
+    };
+    var response = body;
+
+    var req = {
+      ...helper.reqGeneral,
+      body: body,
+    }
+
+    var bodyModificar = {
+      _id: 1,
+      correoPresidente: "ddd@gmail.com",
+      ...body
+    }
+
+    var buscarFederacion = {
+      params: { idFederacion: 1}
+    }
+    var reqModificarFederacion = { 
+      ...buscarFederacion,
+      body: bodyModificar,
+    }
+
     it('debería salvar Federación', () => {
       mockingoose(model).toReturn(response, 'save');
       mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
@@ -22,19 +44,261 @@ describe('Competencia Atleta', () => {
               expect(JSON.parse(JSON.stringify(res))).toMatchObject(response)
             });
     });
-  });
 
-var req = {
-    locals: helper.locals,
-    body: body,
-    token: 'd89fgk'
-}
-
-describe('Crear Federación', () => { 
-    it('returns a 200 response', async () => {
+    it('Crear federación', async () => {
         mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
         const { res } = await expressRequestMock(controlador.crearFederacion, req, helper.resp)
         const { token, datos } = JSON.parse(res._getData());
-      expect(res.statusCode).toEqual(200)
+        expect(res.statusCode).toEqual(200);
+        expect(datos.codigo).toBeLessThan(0);
+        expect(datos.exito).toBeTruthy();
+    });
+
+    it('Modificar federación', async () => {
+      mockingoose(model).toReturn(bodyModificar, 'findOneAndUpdate')
+      const { res } = await expressRequestMock(controlador.modificarFederacion, reqModificarFederacion, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy();
+      });
+
+    it('Leer Federacion', async () => {
+      mockingoose(model).toReturn(bodyModificar, 'findOne')
+      const { res } = await expressRequestMock(controlador.leerFederacion, buscarFederacion, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy();
+      });
+
+    it('Lista Todas Federaciones', async () => {
+      mockingoose(model).toReturn([bodyModificar], 'find')
+      const { res } = await expressRequestMock(controlador.listaTodasFederaciones, helper.reqGeneral, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy();
+      });
+
+    it('Leer Federacion Activa', async () => {
+      mockingoose(model).toReturn(bodyModificar, 'findOne')
+      const { res } = await expressRequestMock(controlador.leerFederacionActiva, buscarFederacion, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy();
+      });
+
+    it('Lista Federaciones Activas', async () => {
+      mockingoose(model).toReturn([bodyModificar], 'find')
+      const { res } = await expressRequestMock(controlador.listaFederacionesActivas, helper.reqGeneral, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy();
+      })
+});
+
+
+describe('Deporte', () => {
+
+  var body = {
+      nombre: "Natación",
+      federacion: 1,
+  };
+  var response = body;
+
+  var req = {
+  ...helper.reqGeneral,
+  body: body,
+  }
+
+  var bodyModificar = {
+    _id: 1,
+    activo: true,
+    ...body
+  }
+
+  var buscarDeporte = {
+    params: { idDeporte: 1, idFederacion: 1}
+  }
+
+  var reqModificarDeporte = { 
+   ...buscarDeporte,
+  body: bodyModificar,
+  }
+
+
+
+  it('Debería salvar Deporte', () => {
+    mockingoose(deporte).toReturn(response, 'save');
+    mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
+    return deporte
+          .create(body).then((res) => {
+            expect(JSON.parse(JSON.stringify(res))).toMatchObject(response)
+          });
+  });
+
+  it('Crear Deporte', async () => {
+      mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
+      const { res } = await expressRequestMock(controlador.crearDeporte, req, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy();
+  });
+
+  it('Modificar Deporte', async () => {
+    mockingoose(deporte).toReturn(bodyModificar, 'findOneAndUpdate')
+    const { res } = await expressRequestMock(controlador.modificarDeporte, reqModificarDeporte, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    });
+
+  it('Leer Deporte', async () => {
+    mockingoose(deporte).toReturn(bodyModificar, 'findOne')
+    const { res } = await expressRequestMock(controlador.leerDeporte, buscarDeporte, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    });
+
+  it('Lista Todos Deportes', async () => {
+    mockingoose(deporte).toReturn([bodyModificar], 'find')
+    const { res } = await expressRequestMock(controlador.listarDeportes, helper.reqGeneral, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    });
+
+  it('Leer Deporte Activo', async () => {
+    mockingoose(deporte).toReturn([bodyModificar], 'find')
+    const { res } = await expressRequestMock(controlador.leerDeporteActiva, buscarDeporte, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    });
+
+  it('Lista Deportes Activos', async () => {
+    mockingoose(deporte).toReturn([bodyModificar], 'find')
+    const { res } = await expressRequestMock(controlador.listarDeportesActivas, helper.reqGeneral, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    })
+
+  it('Lista Deportes X Federación', async () => {
+    mockingoose(deporte).toReturn([bodyModificar], 'find')
+    const { res } = await expressRequestMock(controlador.listarDeportesXFederacion, buscarDeporte, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    })
+
+  it('Lista Deportes X Federación Activos', async () => {
+    mockingoose(deporte).toReturn([bodyModificar], 'find')
+    const { res } = await expressRequestMock(controlador.listarDeportesActivasXFederacion, buscarDeporte, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
     })
 })
+
+
+
+describe('Pruebas', () => {
+
+  var body = {
+      nombre: " Hit 1 Natación",
+      deporte: 1,
+      tipo: 1,
+      activo: true
+  };
+  var response = body;
+
+  var req = {
+    ...helper.reqGeneral,
+    params: {idDeporte: 1},
+    body: body,
+  }
+
+  var bodyModificar = {
+    _id: 1,
+    ...body
+  }
+
+  var buscarPruebas = {
+    params: {idDeporte: 1}
+  }
+  var reqModificarPrueba = { 
+    ...buscarPruebas,
+    body: bodyModificar,
+  }
+
+  it('debería salvar prueba', () => {
+    mockingoose(prueba).toReturn(response, 'save');
+    mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
+    return prueba
+          .create(body).then((res) => {
+            expect(JSON.parse(JSON.stringify(res))).toMatchObject(response)
+          });
+  });
+
+  it('Crear prueba', async () => {
+    mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
+    mockingoose(deporte).toReturn(response, 'findOne');
+    const { res } = await expressRequestMock(controlador.insertarPrueba, req, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+});
+
+  it('Crear prueba existente', async () => {
+      mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
+      mockingoose(deporte).toReturn(response, 'findOne');
+      mockingoose(prueba).toReturn(response, 'findOne');
+      const { res } = await expressRequestMock(controlador.insertarPrueba, req, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeGreaterThan(0);
+      expect(datos.exito).toBeFalsy();
+  });
+
+  it('Modificar prueba', async () => {
+    mockingoose(prueba).toReturn(undefined, 'findOne');
+    mockingoose(prueba).toReturn(bodyModificar, 'findOneAndUpdate')
+    const { res } = await expressRequestMock(controlador.modificarPrueba, reqModificarPrueba, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    });
+
+  it('Lista Todas Pruebas', async () => {
+    mockingoose(prueba).toReturn([bodyModificar], 'find')
+    const { res } = await expressRequestMock(controlador.listarPruebas, buscarPruebas, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    });
+
+  it('Lista Pruebas Activas', async () => {
+    mockingoose(prueba).toReturn([bodyModificar], 'find')
+    const { res } = await expressRequestMock(controlador.listarPruebasActivas, buscarPruebas, helper.resp)
+    const { token, datos } = JSON.parse(res._getData());
+    expect(res.statusCode).toEqual(200);
+    expect(datos.codigo).toBeLessThan(0);
+    expect(datos.exito).toBeTruthy();
+    })
+});
