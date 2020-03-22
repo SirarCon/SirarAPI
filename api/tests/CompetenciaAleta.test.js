c6255d49ba39c1d848a46148587f0c7895b83270
Sirar <sirar2018@hotmail.com>
2020-03-21 19:23:52 -0600

    Tests completos casos positivos competencia atleta

--- api/tests/CompetenciaAleta.test.js
+++ api/tests/CompetenciaAleta.test.js
@@ -4,9 +4,13 @@ contador = require("../models/ContadorModel"),
 evento = require("../models/EventoModel"),
 model = require('../models/CompetenciaAtletaModel'),
 AtletaC = require('../models/AtletaCompetidorModel');
-prueba = require("../models/PruebaModel"),
+pruebaModel = require("../models/PruebaModel"),
 controller = require("../controllers/CompetenciaAtletaController"),
 expressRequestMock = require('express-request-mock');
+
+
+
+describe('Competencia Atleta', () => {
 
 var body = {
         evento: 1,
@@ -17,21 +21,162 @@ var body = {
         genero: 1,
         descripcion: 'Hit 1',
         fase: 2,
-        activo: 1
-}
-var response= {
-    evento: 1,
-    prueba: 1,
-    fechaHora: '2016-08-05T08:00:00.000Z',
-    ciudad: 'Tokio',
-    estadio: 'Tokio Stadium',
-    genero: 1,
-    descripcion: 'Hit 1',
-    fase: 2,
-    activo: true
+        activo: true
 }
 
-describe('Competencia Atleta', () => {
+var reqEliminarAtletaCompetencia = {
+  ...helper.reqGeneral,
+  params: {idAtletaCompetencia: 1}
+}
+
+var responseFindOneAC = {
+  _id: 1,
+  ...body,
+}
+
+var response= body
+var req = {
+  ...helper.reqGeneral,
+  body: body,
+}
+var eventRes= {
+  "_id" : 1,
+  "nombre" : "Juegos Olímpicos de Río de Janeiro 2016",
+  "fechaInicio" : "2016-08-05T00:00:00Z", 
+  "fechaFinal" : "2016-08-21T00:00:00Z",
+  "ciudad" : "Río",
+  "pais" : 76,
+  "fotoUrl" : "imagenes/imagenesEventos/5c8719d7d3cb6b0015814b91.jpeg",
+  "activo" : 1,
+  "nombreNormalizado" : "juegosolimpicosderiodejaneiro2016"
+}
+
+var pruebaRes={
+          "id": 1,
+          "nombre": "200 mts",
+          "deporte": 2,
+          "activo": 1
+}
+
+var atletaCompetidorBody = {
+  atleta: 1,
+  competencia: 1,
+}
+var reqCrearatletaCompetidor = {
+  ...helper.reqGeneral,
+  body: atletaCompetidorBody,
+}
+
+var reqListarAtletasCompetencia ={
+  ...helper.reqGeneral,
+  params:{ idCompetencia: 1,
+  }
+}
+var respGroupListarAtletasCompetencia=[
+  {      _idAtleta: 1, 
+          nombre: 'Atleta prueba', 
+          marcadores: {
+              type:{
+                    mteros: 100
+                  }
+          }}]
+
+var reqListarDeportesXEvento ={
+  ...helper.reqGeneral,
+  params:{ idEvento: 1} 
+}
+
+var {prueba, ...bodyNoPrueba} = body;
+
+var respfinddeportesXevento = [
+  { prueba: {deporte: 1},
+   ...bodyNoPrueba 
+  }
+]
+
+var reqListarCategoriasDeporte = {
+  ...helper.reqGeneral,
+  params: {idDeporte: 1,
+           idEvento: 1
+  }
+}
+var resCategoriasXDeporte = [{
+  prueba: 1,
+  nombre: "100 Mts",
+  genero: 1
+}]
+
+var reqlistarFasesxPruebaEvento = {
+  ...helper.reqGeneral,
+  params:{ idEvento: 1,
+           idPrueba: 1,
+           genero: 1
+  }
+}
+
+var reslistarFasesxPruebaEvento = [{
+fase:{_id: 1, descripcion: "Primera fase"}
+}]
+
+
+var reqlistarCompetenciasEventoPruebaFase = {
+  ...helper.reqGeneral,
+  params:{ idEvento: 1,
+           idPrueba: 1,
+           genero: 1,
+           fase: 1
+  }
+}
+
+var reslistarCompetenciasEventoPruebaFase = [{
+...body
+}]
+
+var reqlistarDeportesEventosAtleta = {
+  ...helper.reqGeneral,
+  params:{ idAtleta: 1,
+           idEvento: 1
+  }
+}
+var reslistarDeportesEventosAtleta = [{
+  _id: 1
+}]
+
+var reqlistarPruebasDeporteEventosAtleta = {
+  ...helper.reqGeneral,
+  params:{ idAtleta: 1,
+           idEvento: 1,
+           idDeporte: 1,
+  }
+}
+
+var reslistarPruebasDeporteEventosAtleta =[{
+    _id: 1,
+    nombre: "100 mts",
+    nombreNormalizado: "100mts"
+}]
+
+var reqlistarCompetenciasPorPruebaAtleta = {
+  ...helper.reqGeneral,
+  params:{ idAtleta: 1,
+           idEvento: 1,
+           idPrueba: 1,
+  }
+}
+
+var reslistarCompetenciasPorPruebaAtleta =[{
+  competencia:{ 
+    _id: 1,
+    descripcion: "100 mts",
+    fase: {
+      id: 1,
+      descripcion: "Primera Fase",
+      siglas: "PF"
+      }
+  }
+}]
+
+
     it('debería salvar competencia atleta', () => {
       mockingoose(model).toReturn(response, 'save');
       mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
@@ -40,39 +185,117 @@ describe('Competencia Atleta', () => {
               expect(JSON.parse(JSON.stringify(res))).toMatchObject(response)
             });
     });
-  });
 
-var req = {
-    locals: helper.locals,
-    body: body,
-    token: 'd89fgk'
-}
-var eventRes= {
-    "_id" : 1,
-    "nombre" : "Juegos Olímpicos de Río de Janeiro 2016",
-    "fechaInicio" : "2016-08-05T00:00:00Z", 
-    "fechaFinal" : "2016-08-21T00:00:00Z",
-    "ciudad" : "Río",
-    "pais" : 76,
-    "fotoUrl" : "imagenes/imagenesEventos/5c8719d7d3cb6b0015814b91.jpeg",
-    "activo" : 1,
-    "nombreNormalizado" : "juegosolimpicosderiodejaneiro2016"
-}
 
-var pruebaRes={
-            "id": 1,
-            "nombre": "200 mts",
-            "deporte": 2,
-            "activo": 1
-}
-
-describe('Crear Competencia Atleta ', () => { 
-    it('returns a 200 response', async () => {
+    it('Crea Competencia Atleta', async () => {
         mockingoose(evento).toReturn(eventRes, 'findOne')
-        mockingoose(prueba).toReturn(pruebaRes, 'findOne')
+        mockingoose(pruebaModel).toReturn(pruebaRes, 'findOne')
         mockingoose(contador).toReturn({sequence_value: 1}, 'findOneAndUpdate')
         const { res } = await expressRequestMock(controller.crearCompetencia, req, helper.resp)
         const { token, datos } = JSON.parse(res._getData());
-      expect(res.statusCode).toEqual(200)
-    })
-})
+        expect(res.statusCode).toEqual(200);
+        expect(datos.codigo).toBeLessThan(0);
+        expect(datos.exito).toBeTruthy();    
+      });
+
+    it('Modificar Competencia Atleta', async () => {
+      mockingoose(model).toReturn(eventRes, 'findOneAndUpdate')
+      const { res } = await expressRequestMock(controller.modificarCompetencia, req, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy();  
+    });
+
+    it('Ingresar Atleta a Competencia', async () => {
+      mockingoose(model).toReturn(responseFindOneAC, 'findOne')
+      mockingoose(AtletaC).toReturn(0, 'estimatedDocumentCount')
+      const { res } = await expressRequestMock(controller.ingresarAtletaACompetencia, reqCrearatletaCompetidor, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Eliminar Atleta de Competencia', async () => {
+      mockingoose(AtletaC).toReturn(responseFindOneAC, 'findOneAndRemove')
+      const { res } = await expressRequestMock(controller.eliminarAtletaDeCompetencia, reqEliminarAtletaCompetencia, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Atletas en Competencia', async () => {
+      mockingoose(AtletaC).toReturn(respGroupListarAtletasCompetencia, 'aggregate')
+      const { res } = await expressRequestMock(controller.listarAtletasCompetencia, reqListarAtletasCompetencia, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Deportes por Evento', async () => {
+      model.schema.path('prueba', Object)//Emula el populate, sino retorna el objeto sin prueba  
+      mockingoose(model).toReturn(respfinddeportesXevento, 'find')
+      const { res } = await expressRequestMock(controller.listarDeportesXEvento, reqListarDeportesXEvento, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Categorías por Deporte', async () => {
+      mockingoose(model).toReturn(resCategoriasXDeporte, 'aggregate')
+      const { res } = await expressRequestMock(controller.listarDeportesXEvento, reqListarCategoriasDeporte, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Fases por Prueba en evento', async () => {
+      mockingoose(model).toReturn(reslistarFasesxPruebaEvento, 'aggregate')
+      const { res } = await expressRequestMock(controller.listarFasesxPruebaEvento, reslistarFasesxPruebaEvento, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Fases por Prueba en Evento y Fase', async () => {
+      mockingoose(model).toReturn(reslistarCompetenciasEventoPruebaFase, 'find')
+      const { res } = await expressRequestMock(controller.listarCompetenciasEventoPruebaFase, reqlistarCompetenciasEventoPruebaFase, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Fases por Prueba en Evento y Fase', async () => {
+      mockingoose(AtletaC).toReturn(reslistarDeportesEventosAtleta, 'aggregate')
+      const { res } = await expressRequestMock(controller.listarDeportesEventosAtleta, reqlistarDeportesEventosAtleta, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Pruebas de atleta según deprte en Evento ', async () => {
+      mockingoose(AtletaC).toReturn(reslistarPruebasDeporteEventosAtleta, 'aggregate')
+      const { res } = await expressRequestMock(controller.listarPruebasDeporteEventosAtleta, reqlistarPruebasDeporteEventosAtleta, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+
+    it('Listar Pruebas de atleta según deprte en Evento ', async () => {
+      mockingoose(AtletaC).toReturn(reslistarCompetenciasPorPruebaAtleta, 'aggregate')
+      const { res } = await expressRequestMock(controller.listarCompetenciasPorPruebaAtleta, reqlistarCompetenciasPorPruebaAtleta, helper.resp)
+      const { token, datos } = JSON.parse(res._getData());
+      expect(res.statusCode).toEqual(200);
+      expect(datos.codigo).toBeLessThan(0);
+      expect(datos.exito).toBeTruthy(); 
+    });
+});
