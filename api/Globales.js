e17343d00bbbd5b6bf01c7d9c87208ae9bb1b95a
Sirar <sirar2018@hotmail.com>
2020-08-13 21:20:28 -0600

    Cambio de Bd

--- api/Globales.js
+++ api/Globales.js
@@ -33,7 +33,7 @@ function inicializarGLobal(variableGlobal, valor){
 //module.exports.nombreBD = inicializarGLobal(nombreBD, "mongodb://localhost/Sirar")
 //module.exports.nombreBDTest = inicializarGLobal(nombreBDTest, "mongodb://localhost/SirarTest")
 //desarrollo:
-//module.exports.nombreBD = inicializarGLobal(nombreBD, "mongodb://root:sirarcon1234@ds121871.mlab.com:21871/sirardb") 
+module.exports.nombreBD = inicializarGLobal(nombreBD, "mongodb://root:sirarcon1234@ds121871.mlab.com:21871/sirardb") 
 //pruebas:
 //module.exports.nombreBD = inicializarGLobal(nombreBD, "mongodb://root:sirarcon1234@ds245132.mlab.com:45132/sirardbpruebas") 
 module.exports.tokenGeneral = inicializarGLobal(tokenGeneral, "d89fgk");
